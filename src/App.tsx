437ccc5361d2454280fbcece0600edd3b4fb351a
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2024-01-04 21:29:32 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { ReactElement } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+
 import SocialLogin from "./pages/socialLogin";
+import Home from "./pages/Home";
 const App = (): ReactElement => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="redirect" element={<SocialLogin />} />
+        <Route path="/auth/googleLogin" element={<SocialLogin />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/socialLogin.tsx
+++ src/pages/socialLogin.tsx
@@ -1,20 +1,31 @@
 import { ReactElement, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import useAuthStore from "../store/auth";
+
 const SocialLogin = (): ReactElement => {
+
   const login = useAuthStore((state) => state.login);
   const navigate = useNavigate();
+
   useEffect(() => {
     const url = new URL(window.location.href);
     const hash = url.hash;
     if (!hash) return;
     // 주소에서 acesstoken 따오기
-    // 코드 출처 https://velog.io/@tkdfo93/%EA%B5%AC%EA%B8%80-OAuth2.0-Final-Project
+    
     const accessToken = hash.split("=")[1].split("&")[0];
+
     login(accessToken);
+
     navigate("/");
+
   }, [login, navigate]);
-  return <div></div>;
+
+  return (
+    <div>
+
+    </div>
+  );
 };
 
 export default SocialLogin;
